6e2c5e8cea7130c63d34acf15c4e17213ece14d9
Filco Martin <martin@apt.id>
2022-09-12 16:40:14 -0700

    export abis

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@
 
 import { AptIDClient } from "./apt_id_client";
 import { DotAptClient } from "./dot_apt_client";
+import * as LocalAbis from "./abis/local/apt_id_abis";
+import * as DevnetAbis from "./abis/devnet/apt_id_abis";
 
 export { AptIDClient };
 export { DotAptClient };
+export { LocalAbis };
+export { DevnetAbis };
